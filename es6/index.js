988be72bffee3ac19b017a6d47a7666d87a182eb
Sven Gerhardt <sv.gerhardt@googlemail.com>
2016-12-02 00:33:01 +0100

    added transaction support

--- es6/index.js
+++ es6/index.js
@@ -1,36 +1,25 @@
 let mysql = require("mysql");
 
-class Database {
+class MySQLWrapper {
 
     constructor(config) {
         this.config = config || {};
-        this.pool = null;
-    }
-
-    query(sql, values) {
-        return new Promise((resolve, reject) => {
-            this.getConnection((error, connection) => {
-                if (error) {
-                    return reject(error);
-                }
-                connection.query(sql, values, (error, rows) => {
-                    this.endConnection(connection);
-                    if (error) {
-                        return reject(error);
-                    }
-                    resolve(rows);
-                });
-            });
-        });
+        this._transactionConnection = null;
+        this._pool = null;
     }
 
     getConnection(callback) {
+
+        if (this._transactionConnection) {
+            return callback(null, this._transactionConnection);
+        }
+
         let options = this.config.connection;
         if (this.config.pooling) {
-            if (this.pool == null) {
-                this.pool = mysql.createPool(options);
+            if (this._pool == null) {
+                this._pool = mysql.createPool(options);
             }
-            this.pool.getConnection(callback);
+            this._pool.getConnection(callback);
         } else {
             let connection = mysql.createConnection(options);
             connection.connect( (error) => {
@@ -41,15 +30,81 @@ class Database {
 
     endConnection(connection) {
         if (this.config.pooling) {
-            if (this.pool && connection) {
+            if (this._pool && connection) {
                 connection.release();
+                this._transactionConnection = null;
             }
         } else {
             if (connection) {
-                connection.end();
+                connection.end((error) => {
+                    this._transactionConnection = null;
+                });
             }
         }
     }
+
+    query(sql, values) {
+        return new Promise((resolve, reject) => {
+            this.getConnection((error, connection) => {
+                if (error) {
+                    return reject(error);
+                }
+                connection.query(sql, values, (error, result) => {
+                    if (!this._transactionConnection) {
+                        this.endConnection(connection);
+                    }
+                    if (error) {
+                        if (this._transactionConnection) {
+                            return this._transactionConnection.rollback(() => {
+                                this.endConnection(this._transactionConnection);
+                                reject(error);
+                            });
+                        } else {
+                            return reject(error);
+                        }
+                    }
+                    resolve(result);
+                });
+            });
+        });
+    }
+
+    beginTransaction() {
+        return new Promise((resolve, reject) => {
+            this.getConnection((error, connection) => {
+                this._transactionConnection = connection;
+                if (error) {
+                    reject(error);
+                } else {
+                    this._transactionConnection.beginTransaction((error) => {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            resolve();
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    endTransaction() {
+        return new Promise((resolve, reject) => {
+            if (this._transactionConnection) {
+                this._transactionConnection.commit((error) => {
+                    if (error) {
+                        return this._transactionConnection.rollback(() => {
+                            this.endConnection(this._transactionConnection);
+                            reject(error);
+                        });
+                    } else {
+                        this.endConnection(this._transactionConnection);
+                    }
+                });
+            }
+            resolve();
+        });
+    }
 }
 
-module.exports.Database = Database;
+module.exports = MySQLWrapper;
